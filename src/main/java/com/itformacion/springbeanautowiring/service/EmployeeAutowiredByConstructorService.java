690fca0b34ff4e866eaa4ec7e73155ef68e18267
escofet <escofet@opensuselinux>
2017-05-07 12:26:52 +0200

    Third commit

---
+++ src/main/java/com/itformacion/springbeanautowiring/service/EmployeeAutowiredByConstructorService.java
@@ -0,0 +1,19 @@
+package com.itformacion.springbeanautowiring.service;
+
+import com.itformacion.springbeanautowiring.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class EmployeeAutowiredByConstructorService {
+    private Employee employee;
+
+    //Autowired annotation on Constructor is equivalent to autowire="constructor"
+    @Autowired(required=false)
+    public EmployeeAutowiredByConstructorService(@Qualifier("employee") Employee emp){
+            this.employee=emp;
+    }
+
+    public Employee getEmployee() {
+            return this.employee;
+    }
+}
---
+++ src/main/java/com/itformacion/springbeanautowiring/service/EmployeeService.java
@@ -0,0 +1,30 @@
+package com.itformacion.springbeanautowiring.service;
+
+import com.itformacion.springbeanautowiring.model.Employee;
+
+public class EmployeeService {
+    private Employee employee;
+
+    // constructor is used for autowire by constructor
+    public EmployeeService(Employee emp) {
+        System.out.println("Autowiring by constructor used");
+        this.employee = emp;
+    }
+    
+    /*
+    Default constructor to avoid BeanInstantiationException 
+    for autowire byName or byType
+    */
+    public EmployeeService() {
+        System.out.println("Default Constructor used");
+    }
+    
+    // used for autowire byName and byType
+    public void setEmployee(Employee emp) {
+        this.employee = emp;
+    }
+
+    public Employee getEmployee() {
+        return this.employee;
+    }
+}
